89a375d0b3570201e13acc7710e5da017061454d
Basma Almajayda <basmaalmajayda@gmail.com>
2023-05-05 01:33:52 +0300

    Patient screens is done

---
+++ app/src/main/java/com/example/palliativecareapp/patient/PatientNotifications.kt
@@ -0,0 +1,12 @@
+package com.example.palliativecareapp.patient
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.palliativecareapp.R
+
+class PatientNotifications : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_patient_notifications)
+    }
+}
