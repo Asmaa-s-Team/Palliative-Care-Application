aefdf83f3bb71e4132aa13ed7621149699db6e06
Basma Almajayda <basmaalmajayda@gmail.com>
2023-05-05 01:35:17 +0300

    Patient screens is done

--- app/src/main/java/com/example/palliativecareapp/patient/PatientTopic.kt
+++ app/src/main/java/com/example/palliativecareapp/patient/PatientTopic.kt
@@ -1,12 +1,12 @@
-package com.example.palliativecareapp.doctor
+package com.example.palliativecareapp.patient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.palliativecareapp.R
 
-class DoctorProfile : AppCompatActivity() {
+class PatientTopic : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_doctor_profile)
+        setContentView(R.layout.activity_patient_topic)
     }
 }
